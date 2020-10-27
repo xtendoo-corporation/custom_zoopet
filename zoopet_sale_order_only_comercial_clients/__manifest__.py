7ffa33069f52e0f66e9f75605c52058e63c8aa70
Dani <dani.xtendoo@gmail.com>
2020-10-27 12:52:54 +0100

    añadido zoopet sale order only comercial clients

---
+++ zoopet_sale_order_only_comercial_clients/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+###################################################################################
+#
+#
+###################################################################################
+
+{
+    "name": "Zoopet Sale order only comercial clients",
+    "summary": """Zoopet Sale order only comercial clients""",
+    "version": "12.0.1.0.0",
+    "description": """Zoopet Sale order only comercial clients""",
+    "author": "DDL",
+    "company": "Xtendoo",
+    "website": "http://www.xtendoo.com",
+    "category": "Extra Tools",
+    "depends": ["sale",
+                "zoopet_administration"],
+    "license": "AGPL-3",
+    "data": ["security/security.xml"],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+}
---
+++ zoopet_sale_order_only_comercial_clients/models/sale_order.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, _
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.onchange("partner_id")
+    def _get_domain(self):
+        if self.env["res.users"].has_group(
+                "zoopet_administration.comercial_group"
+            ):
+            domain = {
+                "partner_id": [
+                    ("user_id", "=", self.env.user.id),
+                    ("parent_id", "=", False),
+                ]
+            }
+
+        else:
+            domain = {
+                "partner_id": [("customer", "=", True), ("parent_id", "=", False)]
+            }
+        print("DOMINIO**************************")
+        print(domain)
+
+        return {"domain": domain}
+
