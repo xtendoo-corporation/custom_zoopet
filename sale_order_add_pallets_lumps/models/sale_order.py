0cb5234824529da2eb1148d198526a2235db1e26
Dani <dani.xtendoo@gmail.com>
2020-10-27 14:15:25 +0100

    traducción sale_order_add_pallets_lumps

--- sale_order_add_pallets_lumps/models/sale_order.py
+++ sale_order_add_pallets_lumps/models/sale_order.py
@@ -4,7 +4,7 @@ from odoo import api, models, fields
 
 
 class SaleOrder(models.Model):
-    _inherit = ["sale.order", "administrator.mixin.rule"]
+    _inherit = ["sale.order"]
     _name = "sale.order"
 
     lumps_number= fields.Integer(string='Lumps Number', store=True)
