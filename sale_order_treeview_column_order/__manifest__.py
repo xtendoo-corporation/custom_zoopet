374a0e6fdc184027b4d6ccdce1b926730bc939fd
dani <dani.xtendoo@gmail.com>
2021-11-16 12:42:50 +0100

    [ADD]Añadido sale_order_treeview_column_order

---
+++ sale_order_treeview_column_order/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+
+
+{
+    "name": "sale_order_treeview_column_order",
+    "summary": """Orden de las columnas de sale pedidos y presupuestos de ventas""",
+    "version": "13.0.1.0.2",
+    "description": """Orden de las columnas de sale pedidos y presupuestos de ventas""",
+    "author": "DDL-Xtendoo",
+    "company": "Xtendoo",
+    "website": "https://xtendoo.es/",
+    "category": "Extra Tools",
+    "depends": [
+        "base",
+        "sale",
+        "zoopet_sale_order_tags",
+        "sale_order_weight",
+    ],
+    "license": "AGPL-3",
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "qweb": [
+    ],
+    "installable": True,
+    "auto_install": False,
+}
