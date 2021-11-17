05388599658f509cde8b0ade010f36d5b8827383
dani <dani.xtendoo@gmail.com>
2021-11-17 14:44:56 +0100

    [MIG] Migrado módulo zoopet_sale_order_only_comercial_clients

---
+++ zoopet_sale_order_only_comercial_clients/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+###################################################################################
+#
+#
+###################################################################################
+
+{
+    "name": "Zoopet Sale order only comercial clients",
+    "summary": """Zoopet sale order only comercial clients""",
+    "version": "13.0.1.0.0",
+    "description": """Zoopet Sale order only comercial clients""",
+    "author": "Dani Domínguez",
+    "company": "Xtendoo",
+    "website": "https://xtendoo.es/",
+    "category": "Extra Tools",
+    "depends": [
+                "sale",
+                "zoopet_administration"
+                ],
+    "license": "AGPL-3",
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+}
