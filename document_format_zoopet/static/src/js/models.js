bedb69414cd7b32c9aa34c057a2e0f17b75b099f
dani <dani.xtendoo@gmail.com>
2022-03-08 10:08:07 +0100

    [MOD] Arreglada herencia del ticket del pos y cambios en ticket

--- document_format_zoopet/static/src/js/models.js
+++ document_format_zoopet/static/src/js/models.js
@@ -8,7 +8,7 @@ odoo.define("pos_tax_name.models", function (require) {
         get_tax_name: function(){
             var details = {};
             var fulldetails = [];
-    
+
             this.orderlines.each(function(line){
                 var ldetails = line.get_tax_details();
                 for(var id in ldetails){
@@ -17,18 +17,18 @@ odoo.define("pos_tax_name.models", function (require) {
                     }
                 }
             });
-    
+
             for(var id in details){
                 if(details.hasOwnProperty(id)){
                     fulldetails.push({amount: details[id], tax: this.pos.taxes_by_id[id], name: this.pos.taxes_by_id[id].name});
                 }
             }
-    
+
             return fulldetails;
         },
-        
+
     });
-    
+
     return models;
 });
 
@@ -39,7 +39,11 @@ odoo.define("pos_unit_name.models", function (require) {
 
     var models = require("point_of_sale.models");
     var field_utils = require('web.field_utils');
-    
+    var utils = require('web.utils');
+
+    var round_di = utils.round_decimals;
+    var round_pr = utils.round_precision;
+
     var _super = models.Orderline.prototype;
     models.Orderline = models.Orderline.extend({
         get_unit_name: function(){
@@ -52,12 +56,35 @@ odoo.define("pos_unit_name.models", function (require) {
         get_quant_2_decimals: function(){
             var quant = parseFloat(this.get_quantity_str());
             quant = field_utils.format.float(quant, {digits: [69, 2]});
-            
+
             return quant;
         },
-        
+
+        get_display_price_one: function(){
+            var rounding = this.pos.currency.rounding;
+            var price_unit = this.get_unit_price();
+            if (this.pos.config.iface_tax_included !== 'total') {
+                return round_pr(price_unit * (1.0 - (this.get_discount() / 100.0)), rounding);
+            } else {
+                var product =  this.get_product();
+                var taxes_ids = product.taxes_id;
+                var taxes =  this.pos.taxes;
+                var product_taxes = [];
+
+                _(taxes_ids).each(function(el){
+                    product_taxes.push(_.detect(taxes, function(t){
+                        return t.id === el;
+                    }));
+                });
+
+                var all_taxes = this.pos.compute_all(product_taxes, price_unit, 1, this.pos.currency.rounding);
+
+                return round_pr(all_taxes.total_included * (1 - this.get_discount()/100), rounding);
+            }
+        },
+
     });
-    
+
     return models;
 });
 
