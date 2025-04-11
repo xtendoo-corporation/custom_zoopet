ea6cd671958913962b7d4f19c569c43c2367b7df
dani <dani.xtendoo@gmail.com>
2025-04-11 09:42:47 +0200

    sync

--- document_format_zoopet/__manifest__.py
+++ document_format_zoopet/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "document_format_Zoopet",
     "summary": """Formatos de documentos Zoopet""",
-    "version": "13.0.1.0.2",
+    "version": "14.0.1.0.2",
     "description": """Formatos de documentos Zoopet""",
     "author": "DDL-Xtendoo",
     "company": "Xtendoo",
