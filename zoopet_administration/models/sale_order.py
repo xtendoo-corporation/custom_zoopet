f1be08f9c54bc25ccf81682fd514b45a2991710d
dani <dani.xtendoo@gmail.com>
2025-08-06 09:34:26 +0200

    sync

--- zoopet_administration/models/sale_order.py
+++ zoopet_administration/models/sale_order.py
@@ -15,19 +15,21 @@ class SaleOrder(models.Model):
             return
         message = ""
         partner_id = self.partner_id
+        #cambiamos la posicion fiscal
+        self.set_fiscal_position()
         title = ("Nota para %s") % partner_id.name
         # Montamos el warning para el comentario de cliente
         if partner_id.comment != False and partner_id.comment != "":
             message = "Nota cliente: %s" % partner_id.comment
-            
+
         # Montamos el warning para el comentario de administradores
-        if self.show_admin_notes and partner_id.admin_comment != False and partner_id.admin_comment != "":        
+        if self.show_admin_notes and partner_id.admin_comment != False and partner_id.admin_comment != "":
             message = message + " \n Nota de administrador: %s" % partner_id.admin_comment
 
         # Montamos el warning para la alerta de venta
         if partner_id.sale_warn in ['warning','block'] and partner_id.sale_warn_msg != False and  partner_id.sale_warn_msg != "":
             message = message + " \n Alerta de Pedido : %s" % partner_id.sale_warn_msg
-            
+
         #Si Se rellena message, se devuelve el warning
         if message != "":
             return {"warning":  {"title": title,
@@ -37,12 +39,13 @@ class SaleOrder(models.Model):
 
     # Método reescribe la posición fiscal en función del tipo de venta
     @api.onchange("type_id")
-    def set_fiscal_position_where_type(self):
-        if self.type_id.fiscal_position_id:
+    def set_fiscal_position(self):
+        if self.partner_id.property_account_position_id:
+            self.fiscal_position_id = self.partner_id.property_account_position_id
+        elif self.type_id.fiscal_position_id and not self.partner_id.property_account_position_id:
             self.fiscal_position_id = self.type_id.fiscal_position_id
-        else:
-            self.fiscal_position_id=self.fiscal_position_id
-           
+
+
 
 
 
